834622d499655cd8e75f6fd2ece900681249c654
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2025-10-09 20:19:01 +0330

    RegisterForm

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Counter from "./components/example/exampleComponent";
 import RegisterForm from "./components/register/registerComponent";
 const Home = () => {
   return (
-    <div className="w-full min-h-screen border-2 border-[blue]">
-      <Counter />
+    <div className="w-full min-h-screen">
+      <RegisterForm />
     </div>
   )
 }
 const About = () => {
   return (
     <div className="w-full min-h-screen border-2 border-[red]">
-      <RegisterForm />
+      <Counter />
     </div>
   )
 }
