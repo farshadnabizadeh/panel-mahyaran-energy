c35875bea4a3556d9bbbb4e16372adb830a3eb18
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2025-10-09 18:33:51 +0330

    installing-Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-const App = () => {
+const Home = () => {
   return (
-    <div className='w-full min-h-screen border-2 border-[red]'>
-      
+    <div className="w-full min-h-screen border-2 border-[blue]">
+
     </div>
   )
 }
+const About = () => {
+  return (
+    <div className="w-full min-h-screen border-2 border-[red]">
 
-export default App
+    </div>
+  )
+}
+export default function App() {
+  return (
+    <div className="p-6">
+      <nav className="space-x-4">
+        <Link to="/" className="text-blue-500">Home</Link>
+        <Link to="/about" className="text-blue-500">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
