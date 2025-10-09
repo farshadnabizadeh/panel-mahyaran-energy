790cca5e5ff87e4e993fbf893a7d2fec29eede03
Farshad Nabizadeh <farshadnabizadeh1993@gmail.com>
2025-10-09 20:54:43 +0330

    signupForm

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Counter from "./components/example/exampleComponent";
 import RegisterForm from "./components/register/registerComponent";
-const Home = () => {
-  return (
-    <div className="w-full min-h-screen">
-      <RegisterForm />
-    </div>
-  )
-}
-const About = () => {
-  return (
-    <div className="w-full min-h-screen border-2 border-[red]">
-      <Counter />
-    </div>
-  )
-}
+
+
 export default function App() {
   return (
     <div className="w-full min-h-screen">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<RegisterForm />} />
+        <Route path="/about" element={<Counter />} />
       </Routes>
     </div>
   );
